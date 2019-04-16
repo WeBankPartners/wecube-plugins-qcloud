e5becd517b8d9b22a663791147ed4aa46f5274e4
v_cyleizeng <v_cyleizeng@webank.com>
2019-04-16 16:35:01 +0800

    [S116446]modify start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo su - root
-docker build -t wecube-plugins:v1 .
-docker run -d -p 9190:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:v1
+sudo docker build -t wecube-plugins:v1 .
+sudo docker run -d -p 9190:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:v1
 
 # mkdir -p ../logs
 # ./wecube-plugins >> ../logs/wecube-plugins.log 2>&1 &
