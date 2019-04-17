cf4eab76fd1802c5183c495b654e032f23049292
jordanzhang(张天军) <jordanzhang@webank.com>
2019-04-17 11:06:16 +0800

    [S116446] Enhance the version naming of docker image

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-current=`date "+%Y-%m-%d %H:%M:%S"`
-timeStamp=`date -d "$current" +%s`
-sudo docker build -t wecube-plugins:$timeStamp .
-sudo docker run -d --name wecube-plugins -p 8081:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$timeStamp
+VERSION=`date +%Y%m%d%H%M%S`
+sudo docker build -t wecube-plugins:$VERSION .
+sudo docker run -d --name wecube-plugins -p 8081:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$VERSION
