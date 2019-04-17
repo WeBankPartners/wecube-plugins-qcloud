1599100097d87012e44a0fc62130d3f7be4154e9
jordanzhang(张天军) <jordanzhang@webank.com>
2019-04-17 13:29:31 +0800

    [S116446] expose docker conf and logs directory

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 VERSION=`date +%Y%m%d%H%M%S`
 docker build -t wecube-plugins:$VERSION .
 docker save wecube-plugins:$VERSION -o wecube-plugins.$VERSION.image
-docker run -d --name wecube-plugins -p 8081:8081 -v $PWD/conf:/home/app/conf -v $PWD/logs:/home/app/logs -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$VERSION
+docker run -d --name wecube-plugins -p 8081:8081 -v $PWD/conf:/home/app/wecube-plugins/conf -v $PWD/logs:/home/app/wecube-plugins/logs -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$VERSION
