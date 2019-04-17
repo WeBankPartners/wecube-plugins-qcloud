ff8fec7bb80d32201c8db8421987daaacc2fefe3
jordanzhang(张天军) <jordanzhang@webank.com>
2019-04-17 11:48:35 +0800

    [S116446] remove the sudo for running the scripts

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION=`date +%Y%m%d%H%M%S`
-sudo docker build -t wecube-plugins:$VERSION .
-sudo docker run -d --name wecube-plugins -p 8081:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$VERSION
+docker build -t wecube-plugins:$VERSION .
+docker run -d --name wecube-plugins -p 8081:8081 -e http_proxy=10.107.100.64:9090 -e https_proxy=10.107.100.64:9090 -e no_proxy="10.107.111.105,10.107.111.88,10.107.119.150,10.107.117.154,10.107.119.52" -it wecube-plugins:$VERSION
--- docker_stop.sh
+++ docker_stop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-IMAGEID=`sudo docker ps -aqf 'name=wecube-plugins'`
-sudo docker rm -f $IMAGEID
+docker rm -f `sudo docker ps -aqf 'name=wecube-plugins'`
 
-sudo docker images | grep -E "wecube-plugins" | awk '{print $3}' | uniq | xargs -I {} docker rmi -f {}
+docker images | grep -E "wecube-plugins" | awk '{print $3}' | uniq | xargs -I {} docker rmi -f {}
