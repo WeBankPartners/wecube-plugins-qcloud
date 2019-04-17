3c55e377a4063defe375ef3334d9f5dc22ad1576
v_cyleizeng <v_cyleizeng@webank.com>
2019-04-17 10:34:10 +0800

    [S116446]1. modify docker images tag by timestamp; 2. add step to remove old docker image

--- docker_stop.sh
+++ docker_stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-IMAGEID=`sudo docker ps -aqf 'name=wecube-plugins-smoke'`
+IMAGEID=`sudo docker ps -aqf 'name=wecube-plugins'`
 sudo docker rm -f $IMAGEID
+
+sudo docker images | grep -E "wecube-plugins" | awk '{print $3}' | uniq | xargs -I {} docker rmi -f {}
