8025e21910f9d8326e4b5b754ea1f611657973b2
v_cyleizeng <v_cyleizeng@webank.com>
2019-04-22 16:31:31 +0800

    [S118560]test logs get keyword

--- plugins/log_search.go
+++ plugins/log_search.go
@@ -88,7 +88,7 @@ func (action *LogGetKeyWordAction) GetKeyWord(input *LogInput) (interface{}, err
 		input.LineNumber = "10"
 	}
 
-	sh := "cat logs/wecube-plugins.log |grep " + input.KeyWord + " -C " + input.LineNumber
+	sh := "cat /home/app/logs/wecube-plugins.log |grep " + input.KeyWord + " -C " + input.LineNumber
 	cmd := exec.Command(sh)
 
 	//创建获取命令输出管道
