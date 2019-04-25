1a5522cde203a04f68ce1ffb064e41ee7a6390b1
v_cyleizeng <v_cyleizeng@webank.com>
2019-04-25 16:00:11 +0800

    [S118560]test

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -3,6 +3,7 @@ package plugins
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -110,10 +111,12 @@ func Process(pluginRequest *PluginRequest) (*PluginResponse, error) {
 	}
 
 	// logrus.Infof("action do with parameters = %v", actionParam)
+	logrus.Info("plugin start time ==>>", time.Now().Format("2016-01-02 15:04:05"))
 	outputs, err := action.Do(actionParam)
 	if err != nil {
 		return &pluginResponse, err
 	}
+	logrus.Info("plugin end time ==>>", time.Now().Format("2016-01-02 15:04:05"))
 
 	pluginResponse.Results = outputs
 
