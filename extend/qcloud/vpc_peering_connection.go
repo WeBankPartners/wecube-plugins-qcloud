0a00918f320a2cc1829a706d4d247d13fa532dee
v_cyleizeng <v_cyleizeng@webank.com>
2019-05-14 17:33:44 +0800

    [S120524]add peerconnection resource check

--- extend/qcloud/vpc_peering_connection.go
+++ extend/qcloud/vpc_peering_connection.go
@@ -90,3 +90,65 @@ func (c *Client) DeletePeeringConnection(request *DeleteVpcPeeringConnectionRequ
 	err = c.Send(request, response)
 	return
 }
+
+type DescribeVpcPeeringConnectionRequest struct {
+	*common.BaseRequest
+	VpcId                 *string `json:"vpcId,omitempty"`
+	PeeringConnectionId   *string `json:"peeringConnectionId,omitempty"`
+	PeeringConnectionName *string `json:"peeringConnectionName,omitempty"`
+	State                 *string `json:"state,omitempty"`
+	Offset                *int    `json:"offset,omitempty"`
+	Limit                 *int    `json:"limit,omitempty"`
+	OrderField            *string `json:"orderField,omitempty"`
+	OrderDirection        *string `json:"orderDirection,omitempty"`
+}
+
+type DescribeVpcPeeringConnectionResponse struct {
+	*common.BaseResponse
+	Code       *int              `json:"code,omitempty"`
+	Message    *string           `json:"message,omitempty"`
+	TotalCount *int              `json:"totalCount,omitempty"`
+	Data       []*PeerConnectSet `json:"data,omitempty"`
+}
+
+type PeerConnectSet struct {
+	VpcId                 *string `json:"vpcId,omitempty"`
+	UnVpcId               *string `json:"unVpcId,omitempty"`
+	PeerVpcId             *string `json:"peerVpcId,omitempty"`
+	UnPeerVpcId           *string `json:"unPeerVpcId,omitempty"`
+	AppId                 *string `json:"appId,omitempty"`
+	PeeringConnectionId   *string `json:"peeringConnectionId,omitempty"`
+	PeeringConnectionName *string `json:"peeringConnectionName,omitempty"`
+	State                 *int    `json:"state,omitempty"`
+	CreateTime            *string `json:"createTime,omitempty"`
+	Uin                   *string `json:"uni,omitempty"`
+	PeerUin               *string `json:"peerUin,omitempty"`
+	UniqVpcId             *string `json:"uniqVpcId,omitempty"`
+	UniqPeerVpcId         *string `json:"uniqPeerVpcId,omitempty"`
+	Region                *string `json:"region,omitempty"`
+	PeerRegion            *string `json:"peerRegion,omitempty"`
+}
+
+func NewDescribeVpcPeeringConnectionRequest() (request *DescribeVpcPeeringConnectionRequest) {
+	request = &DescribeVpcPeeringConnectionRequest{
+		BaseRequest: &common.BaseRequest{},
+	}
+	request.Init().WithApiInfo("vpc", APIVersion, "DescribeVpcPeeringConnections")
+	return
+}
+
+func NewDescribeVpcPeeringConnectionResponse() (response *DescribeVpcPeeringConnectionResponse) {
+	response = &DescribeVpcPeeringConnectionResponse{
+		BaseResponse: &common.BaseResponse{},
+	}
+	return
+}
+
+func (c *Client) DescribeVpcPeeringConnections(request *DescribeVpcPeeringConnectionRequest) (response *DescribeVpcPeeringConnectionResponse, err error) {
+	if request == nil {
+		request = NewDescribeVpcPeeringConnectionRequest()
+	}
+	response = NewDescribeVpcPeeringConnectionResponse()
+	err = c.Send(request, response)
+	return
+}
