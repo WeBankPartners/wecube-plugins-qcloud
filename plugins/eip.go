089735dbb498bca0d072f93b13f5f2caed914c1b
v_cyleizeng <v_cyleizeng@webank.com>
2019-05-30 11:39:18 +0800

    [S118921]debug

--- plugins/eip.go
+++ plugins/eip.go
@@ -144,8 +144,8 @@ func (action *EIPCreateAction) createEIP(eip *EIPInput) (*EIPOutput, error) {
 	}
 	logrus.Info("DescribeAddresses == >>>> 11111")
 	for _, info := range queryEIPResponse.Response.AddressSet {
-		// logrus.Info("info.AddressId == >>>> ", *info.AddressId)
-		// logrus.Info("info.AddressIp == >>>> ", *info.AddressIp)
+		logrus.Info("info.AddressId == >>>> ", *info.AddressId)
+		logrus.Info("info.AddressIp == >>>> ", *info.AddressIp)
 		var eipInfo EIPInfo
 		eipInfo.Id = *info.AddressId
 		eipInfo.EIP = *info.AddressIp
