115dd59f69b85657d4e0f107c5d020e4bf0de397
brankbao <brankbao@webank.com>
2019-10-11 18:19:36 +0800

    36_add_clb_code

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -59,6 +59,7 @@ func init() {
 	RegisterPlugin("eip", new(EIPPlugin))
 	RegisterPlugin("mariadb", new(MariadbPlugin))
 	RegisterPlugin("route-policy", new(RoutePolicyPlugin))
+	RegisterPlugin("clb",new(ClbPlugin))
 
 }
 
