be642e8c6a2e07d32b610d310c532b61d266e901
zgyzgyhero <894526647@qq.com>
2020-09-01 15:04:05 +0800

    Add core auth

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"fmt"
 
 	_ "github.com/WeBankPartners/wecube-plugins-qcloud/plugins/bussiness_plugins/security_group"
 
@@ -12,10 +13,19 @@ import (
 	"github.com/WeBankPartners/wecube-plugins-qcloud/plugins"
 	"github.com/sirupsen/logrus"
 	"github.com/snowzach/rotatefilehook"
+	"io/ioutil"
+	"encoding/base64"
+	"bytes"
+	"github.com/dgrijalva/jwt-go"
+	"strconv"
 )
 
 const (
 	CONF_FILE_PATH = "./conf/app.conf"
+)
+
+var (
+	CoreJwtKey string
 )
 
 func init() {
@@ -52,6 +62,7 @@ func initLogger() {
 }
 
 func initConfig() {
+	CoreJwtKey = os.Getenv("JWT_SIGNING_KEY")
 	conf.InitConfig(CONF_FILE_PATH)
 }
 
@@ -61,11 +72,17 @@ func initRouter() {
 }
 
 func routeDispatcher(w http.ResponseWriter, r *http.Request) {
-	pluginRequest := parsePluginRequest(r)
-	pluginResponse, _ := plugins.Process(pluginRequest)
-	b,_ := json.Marshal(pluginResponse)
-	logrus.Infof("write data to client response=%s", string(b))
-	write(w, pluginResponse)
+	if authCore(r.Header.Get("Authorization")) {
+		pluginRequest := parsePluginRequest(r)
+		pluginResponse, _ := plugins.Process(pluginRequest)
+		b, _ := json.Marshal(pluginResponse)
+		logrus.Infof("write data to client response=%s", string(b))
+		write(w, pluginResponse)
+	}else{
+		logrus.Warnf("Request token illegal ----------------!!")
+		pluginResponse := plugins.PluginResponse{ResultCode:"1",ResultMsg:"Token illegal"}
+		write(w, &pluginResponse)
+	}
 }
 
 func write(w http.ResponseWriter, output *plugins.PluginResponse) {
@@ -90,4 +107,54 @@ func parsePluginRequest(r *http.Request) *plugins.PluginRequest {
 	pluginInput.Parameters = r.Body
 	logrus.Infof("parsed request = %v", pluginInput)
 	return &pluginInput
+}
+
+func authCore(coreToken string) bool {
+	_,err := decodeCoreToken(coreToken, CoreJwtKey)
+	if err == nil {
+		return true
+	}
+	return false
+}
+
+type coreJwtToken struct {
+	User    string    `json:"user"`
+	Expire  int64     `json:"expire"`
+	Roles   []string  `json:"roles"`
+}
+
+func decodeCoreToken(token,key string) (result coreJwtToken,err error) {
+	if strings.HasPrefix(token, "Bearer") {
+		token = token[7:]
+	}
+	if key == "" || strings.HasPrefix(key, "{{") {
+		key = "Platform+Auth+Server+Secret"
+	}
+	keyBytes,err := ioutil.ReadAll(base64.NewDecoder(base64.RawStdEncoding, bytes.NewBufferString(key)))
+	if err != nil {
+		logrus.Error("Decode core token fail,base64 decode error", err)
+		return result,err
+	}
+	pToken,err := jwt.Parse(token, func(*jwt.Token) (interface{}, error) {
+		return keyBytes, nil
+	})
+	if err != nil {
+		logrus.Error("Decode core token fail,jwt parse error", err)
+		return result,err
+	}
+	claimMap,ok := pToken.Claims.(jwt.MapClaims)
+	if !ok {
+		logrus.Error("Decode core token fail,claims to map error", err)
+		return result,err
+	}
+	result.User = fmt.Sprintf("%s", claimMap["sub"])
+	result.Expire,err = strconv.ParseInt(fmt.Sprintf("%.0f", claimMap["exp"]), 10, 64)
+	if err != nil {
+		logrus.Error("Decode core token fail,parse expire to int64 error", err)
+		return result,err
+	}
+	roleListString := fmt.Sprintf("%s", claimMap["authority"])
+	roleListString = roleListString[1:len(roleListString)-1]
+	result.Roles = strings.Split(roleListString, ",")
+	return result,nil
 }
